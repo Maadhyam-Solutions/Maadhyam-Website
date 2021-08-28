cdd3eb077fb995f33b50c649f8e5765aca6ecfe1
Rojen Maharjan <44132059+rojen11@users.noreply.github.com>
2021-08-28 17:59:14 +0545

    feat: mobile nav bar

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,30 +2,49 @@
 var imageDirectory = "assets/images/icon/";
 
 var technologies = [
-    {name: "html5", image: "html5.svg"},
-    {name: "css", image: "css3.svg"},
-    {name: "javascript", image: "javascript.svg"},
-    {name: "boostrap", image: "bootstrap.svg"},
-    {name: "react", image: "react.svg"}
-]
+  { name: "html5", image: "html5.svg" },
+  { name: "css", image: "css3.svg" },
+  { name: "javascript", image: "javascript.svg" },
+  { name: "boostrap", image: "bootstrap.svg" },
+  { name: "react", image: "react.svg" },
+];
 
-var carouselRotation = 360/technologies.length
+var carouselRotation = 360 / technologies.length;
 
-var carouselBody = document.querySelector(".carousel-body");
+var carouselBody = document.querySelector(".carousel-body-js");
 
 for (var i = 0; i < technologies.length; i++) {
-    var imgEl = document.createElement("img");
-    imgEl.src = imageDirectory + technologies[i].image;
+  var imgEl = document.createElement("img");
+  imgEl.src = imageDirectory + technologies[i].image;
 
-    var figEl = document.createElement("figure");
-    figEl.appendChild(imgEl);
+  var figEl = document.createElement("figure");
+  figEl.appendChild(imgEl);
 
-    figEl.classList.add("carousel-item");
+  figEl.classList.add("carousel-item");
 
-    // rotation style
-    var rotationStyle = "rotateY(" + (carouselRotation * i) + "deg) translateZ(300px)";
+  // rotation style
+  var rotationStyle =
+    "rotateY(" + carouselRotation * i + "deg) translateZ(300px)";
 
-    figEl.style.transform = rotationStyle;
+  figEl.style.transform = rotationStyle;
 
-    carouselBody.appendChild(figEl);
+  carouselBody.appendChild(figEl);
 }
+
+// responsive menu
+var menuButton = document.querySelector("#menu-button-js");
+var mobileNav = document.querySelector("#mobile-nav-js");
+var closeButton = document.querySelector("#close-button-js");
+var navButtons = document.querySelector("#nav-res-js");
+
+menuButton.addEventListener("click", function () {
+  mobileNav.classList.remove("hidden");
+});
+
+function hideMenu() {
+  mobileNav.classList.add("hidden");
+}
+
+closeButton.addEventListener("click", hideMenu);
+
+navButtons.addEventListener("click", hideMenu);
